b952b520181f9c3c97ca8a2a68f974c1d2bc08ae
zylz10 <zhangyiling@NicoledeiMac.local>
2017-03-30 16:54:28 +0800

    从UI中抽离业务的MVP架构

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mvpdemo
+//
+//  Created by zhangyiling on 16/11/30.
+//  Copyright © 2016年 zhangyiling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  mvpdemo
+//
+//  Created by zhangyiling on 16/11/30.
+//  Copyright © 2016年 zhangyiling. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "VCController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    float w = self.view.frame.size.width - 30*2;
+    
+    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(30, 100, w , 48)];
+    [btn addTarget:self action:@selector(click:) forControlEvents:UIControlEventTouchUpInside];
+    [btn setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+    [btn setTitle:@"test" forState:UIControlStateNormal];
+    btn.layer.borderColor = [UIColor orangeColor].CGColor;
+    btn.layer.borderWidth = 1.f;
+    [self.view addSubview:btn];
+
+}
+-(void)click:(UIButton *)btn{
+
+    VCController *vc = [[VCController alloc]init];
+//    UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:self];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+-(void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    
+}
+
+@end
